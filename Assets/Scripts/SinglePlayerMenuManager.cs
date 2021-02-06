a1e6b4bfe8c3fca480416f671bce70e602eab665
Christian Kyle <57316283+christiankyle-ching@users.noreply.github.com>
2021-02-06 18:28:14 +0800

    Added Pause Button

--- Assets/Scripts/SinglePlayerMenuManager.cs
+++ Assets/Scripts/SinglePlayerMenuManager.cs
@@ -7,10 +7,37 @@ using UnityEngine.UI;
 
 public class SinglePlayerMenuManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject pauseButton;
+
     private void Awake()
     {
         InitEndMenu();
+        InitPauseMenu();
+
+        pauseButton.GetComponent<Button>().onClick.AddListener(() => PauseGame());
     }
+
+    // PauseGameMenu
+    [SerializeField]
+    private GameObject pauseMenuUI;
+    private Button pause_ResumeButton, pause_RestartButton, pause_MenuButton, pause_QuitButton;
+
+    private void InitPauseMenu()
+    {
+        pauseMenuUI.SetActive(false);
+
+        pause_ResumeButton = pauseMenuUI.transform.Find("Resume").GetComponent<Button>();
+        pause_RestartButton = pauseMenuUI.transform.Find("Restart").GetComponent<Button>();
+        pause_MenuButton = pauseMenuUI.transform.Find("Menu").GetComponent<Button>();
+        pause_QuitButton = pauseMenuUI.transform.Find("Quit").GetComponent<Button>();
+
+        pause_ResumeButton.onClick.AddListener(() => ResumeGame());
+        pause_RestartButton.onClick.AddListener(() => RestartGame());
+        pause_MenuButton.onClick.AddListener(() => GotoMainMenu());
+        pause_QuitButton.onClick.AddListener(() => QuitGame());
+    }
+
 
     // EndGameMenu
     [SerializeField]
@@ -32,6 +59,18 @@ public class SinglePlayerMenuManager : MonoBehaviour
     public void EndGame(PlayerStats stats)
     {
         endMenuUI.SetActive(true);
+    }
+
+    private void ResumeGame()
+    {
+        pauseButton.SetActive(true);
+        pauseMenuUI.SetActive(false);
+    }
+
+    private void PauseGame()
+    {
+        pauseButton.SetActive(false);
+        pauseMenuUI.SetActive(true);
     }
 
     private void RestartGame()
