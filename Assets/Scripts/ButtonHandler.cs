6a548a15cf64ed6d523c21877bceaf38461a9074
John Paul Pineda <57859589+ijohnpaul2000@users.noreply.github.com>
2021-05-26 13:26:20 +0800

    Anims

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ButtonHandler : MonoBehaviour
@@ -33,6 +34,11 @@ public class ButtonHandler : MonoBehaviour
                 break;
         }
         
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene("Main Menu");
     }
 
  
