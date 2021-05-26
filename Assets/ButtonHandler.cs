78c4805e5adce08d7671eae3169c2c9496aa4e4f
John Paul Pineda <57859589+ijohnpaul2000@users.noreply.github.com>
2021-05-26 13:26:43 +0800

    Revert "Anims"
    This reverts commit 6a548a15cf64ed6d523c21877bceaf38461a9074.

--- Assets/ButtonHandler.cs
+++ Assets/ButtonHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ButtonHandler : MonoBehaviour
@@ -34,11 +33,6 @@ public class ButtonHandler : MonoBehaviour
                 break;
         }
         
-    }
-
-    public void BackButton()
-    {
-        SceneManager.LoadScene("Main Menu");
     }
 
  
