91ee33650a35e1c76842701753ebe92f55c6bded
Christian Kyle <57316283+christiankyle-ching@users.noreply.github.com>
2021-06-22 17:21:15 +0800

    Student Name and Section

---
+++ Assets/StudentProfileHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StudentProfileHandler : MonoBehaviour
+{
+    public GameObject
+
+            txtStudentName,
+            dropdownStudentSection;
+
+    public void OnEndEditName(string text)
+    {
+        PlayerPrefs.SetString("Profile_Name", text);
+    }
+
+    public void OnValueChangedSection(int sectionIndex)
+    {
+        PlayerPrefs.SetInt("Profile_Section", sectionIndex);
+    }
+
+    void Awake()
+    {
+        txtStudentName.GetComponent<TMP_InputField>().text =
+            PlayerPrefs.GetString("Profile_Name", "");
+
+        dropdownStudentSection.GetComponent<TMP_Dropdown>().value =
+            PlayerPrefs.GetInt("Profile_Section", 0);
+    }
+}
