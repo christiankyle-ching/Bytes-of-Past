51218ae10aa64b5f4d0899962a736d784635799d
Christian Kyle <57316283+christiankyle-ching@users.noreply.github.com>
2021-10-16 16:10:30 +0800

    Initial Scene Setup

---
+++ Assets/Scripts/MP/MPDragDrop.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MPDragDrop : MonoBehaviour
+{
+    private GameObject canvas; // GameCanvas
+    private GameObject dropzone; // last dropzone the card entered
+
+    private bool isDragging = false;
+    private bool isOverDropZone = false;
+    private Transform startParent;
+    private Vector2 startPos;
+
+    private void Start()
+    {
+        canvas = GameObject.Find("GameCanvas");
+        dropzone = transform.parent.gameObject;
+    }
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = Input.mousePosition;
+            transform.SetParent(canvas.transform, true);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOverDropZone = true;
+        dropzone = collision.gameObject;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOverDropZone = false;
+        dropzone = null;
+    }
+
+    public void OnBeginDrag()
+    {
+        isDragging = true;
+
+        startParent = transform.parent;
+        startPos = transform.position;
+    }
+
+    public void OnEndDrag()
+    {
+        isDragging = false;
+
+        if (isOverDropZone)
+        {
+            transform.SetParent(dropzone.transform, false);
+        } else
+        {
+            transform.SetParent(startParent, false);
+            transform.position = startPos;
+        }
+    }
+}
