6605b28b5a650663eed3101e30ed8b117f904720
Christian Kyle <57316283+christiankyle-ching@users.noreply.github.com>
2021-11-09 14:17:04 +0800

    fix: Improved loading animation

--- Assets/Scripts/GameLoader.cs
+++ Assets/Scripts/GameLoader.cs
@@ -26,7 +26,7 @@ public class GameLoader : MonoBehaviour
         // COMPUTER
         while (!req0.isDone)
         {
-            UpdateProgress(req0.progress, req1.progress, req2.progress);
+            //UpdateProgress(req0.progress, req1.progress, req2.progress);
             yield return null;
         }
         ResourceParser.Instance.SetCards(req0.asset as TextAsset, HistoryTopic.COMPUTER);
@@ -34,23 +34,30 @@ public class GameLoader : MonoBehaviour
         // NETWORKING
         while (!req1.isDone)
         {
-            UpdateProgress(req0.progress, req1.progress, req2.progress);
+            //UpdateProgress(req0.progress, req1.progress, req2.progress);
             yield return null;
         }
         ResourceParser.Instance.SetCards(req1.asset as TextAsset, HistoryTopic.NETWORKING);
 
-        // TODO: Better artificial loading? Because loading is instant
-        yield return new WaitForSecondsRealtime(1f); 
-
         // SOFTWARE
         while (!req2.isDone)
         {
-            UpdateProgress(req0.progress, req1.progress, req2.progress);
+            //UpdateProgress(req0.progress, req1.progress, req2.progress);
             yield return null;
         }
         ResourceParser.Instance.SetCards(req2.asset as TextAsset, HistoryTopic.SOFTWARE);
 
-        UpdateProgress(req0.progress, req1.progress, req2.progress);
+        // Artificial Loading
+        float j = 0;
+        while (j < 1f)
+        {
+            j += 0.01f;
+            UpdateProgress(Mathf.Clamp01(j));
+            yield return new WaitForSecondsRealtime(.01f);
+        }
+
+
+        //UpdateProgress(req0.progress, req1.progress, req2.progress);
         Debug.Log($"Finished Loading Resources... {req0.isDone}, {req1.isDone}, {req2.isDone}");
         Debug.Log($"Finished Loading Resources... {req0.progress}, {req1.progress}, {req2.progress}");
 
@@ -63,6 +70,12 @@ public class GameLoader : MonoBehaviour
 
         progressBar.value = overallProgress;
         progressText.text = $"{Math.Round(overallProgress * 100)} %";
+    }
+
+    void UpdateProgress(float progress)
+    {
+        progressBar.value = progress;
+        progressText.text = $"{Math.Round(progress * 100)} %";
     }
 
 }
