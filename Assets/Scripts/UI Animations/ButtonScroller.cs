43a222bb0bdf22ee3e88b89f39d56d6cf6d37345
Christian Kyle <57316283+christiankyle-ching@users.noreply.github.com>
2021-11-12 16:57:27 +0800

    feat: Button for Scrollers

---
+++ Assets/Scripts/UI Animations/ButtonScroller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(ScrollRect))]
+public class ButtonScroller : MonoBehaviour
+{
+    [Header("UI Buttons")]
+    public Button btnLeft;
+    public Button btnRight;
+
+    [Header("Scroll Settings")]
+    [Range(0f, 100f)] public float scrollSensitivity;
+
+    ScrollRect scroller;
+
+
+    private void Start()
+    {
+        scroller = GetComponent<ScrollRect>();
+
+        btnLeft.onClick.AddListener(() => ScrollContent(-1));
+        btnRight.onClick.AddListener(() => ScrollContent(1));
+    }
+
+    // positive for right, negative for left
+    private void ScrollContent(float direction)
+    {
+        float realDirection = Mathf.Sign(direction) * -1;
+        // Flip the sign again, because it's actually the other way around
+
+        scroller.velocity = new Vector2(realDirection * scrollSensitivity * 100f, 0);
+
+        Debug.Log(realDirection * 1000f);
+    }
+
+    public void OnScrollerPositionChanged(Vector2 position)
+    {
+        btnLeft.interactable = scroller.horizontalNormalizedPosition > 0.25f;
+        btnRight.interactable = scroller.horizontalNormalizedPosition < 0.75f;
+    }
+}
