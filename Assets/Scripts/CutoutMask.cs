4ea19a900305f0d1b138b0a79d4325bb1f2f0381
Christian Kyle <57316283+christiankyle-ching@users.noreply.github.com>
2021-11-17 16:01:16 +0800

    fix: GameObject Anchors

---
+++ Assets/Scripts/CutoutMask.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class CutoutMask : Image
+{
+    public override Material materialForRendering
+    {
+        get
+        {
+            Material material = new Material(base.materialForRendering);
+            material.SetInt("_StencilComp", (int)CompareFunction.NotEqual);
+            return material;
+        }
+
+    }
+}
